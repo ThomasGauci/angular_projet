b367a75f99c4b822beeff056eeb5237261e689c1
tgauci <tgauci@ics.fr>
2021-03-03 10:34:15 +0100

    login + router sur acceuil

--- Projet/app/src/app/app-routing.module.ts
+++ Projet/app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AssignmentsComponent } from './assignments/assignments.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path : '', component: LoginComponent}
+  {path : '', component: LoginComponent},
+  {path :'acceuil', component: AssignmentsComponent}
 ]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Projet/app/src/app/login/login.component.ts
+++ Projet/app/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public login: String;
+  public password: String;
+
+  constructor(private router: Router) { 
+    this.login = "";
+    this.password = "";
+  }
 
   ngOnInit(): void {
   }
-
+  
+  connexion(){
+    console.log(this.login);
+    console.log(this.password);
+    this.router.navigate(['acceuil']);
+  }
 }
