fdc14687d05b23135d5e4ec8e8d2febd2d5da720
tgauci <tgauci@ics.fr>
2021-03-15 16:21:15 +0100

    onglet assignements base

--- Front/app/src/app/assignments/assignments.component.ts
+++ Front/app/src/app/assignments/assignments.component.ts
@@ -35,6 +35,18 @@ export class AssignmentsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  add(){
+    console.log('Bouton cliqué');
+    console.log('Nom = ' + this.nomDevoir);
+    console.log('Date = ' + this.dateDeRendu);
+
+    let nouvelAssignment = new Assignment();
+    nouvelAssignment.nom = this.nomDevoir;
+    nouvelAssignment.dateDeRendu = this.dateDeRendu;
+    nouvelAssignment.rendu = false;
+
+    this.assignments.push(nouvelAssignment);
+  }
   onSubmit(event: { preventDefault: () => void; }) {
     event.preventDefault();
     console.log('Bouton cliqué');
